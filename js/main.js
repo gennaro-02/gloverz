e432ed18182a30a938a6cf22b211329ab935e84b
gennaro-02 <gennarodirosa@abacatania.it>
2024-07-24 20:41:06 +0200

    responsive header - ridimensionate immagini

--- js/main.js
+++ js/main.js
@@ -46,6 +46,14 @@ function filtra(filtro) {
 }
 
 
+function openNav() {
+  
+  var overlay = document.getElementsByClassName("overlay")
+  overlay[0].style.display = "block"
+}
 
-
+function closeNav() {
+  var overlay = document.getElementsByClassName("overlay")
+  overlay[0].style.display = "none"
+ }
 
